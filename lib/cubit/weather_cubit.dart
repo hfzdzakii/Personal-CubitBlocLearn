04d977ca4b1ce7431c1119ec48aee381e84253f2
hfzdzakii <muhammadhafizhdzaki@gmail.com>
2026-03-27 12:33:35 +0700

    Checkpoint Cubit

---
+++ lib/cubit/weather_cubit.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+import 'package:weather_bloc_cubit/data/model/weather.dart';
+import 'package:weather_bloc_cubit/data/weather_repository.dart';
+
+part 'weather_state.dart';
+
+class WeatherCubit extends Cubit<WeatherState> {
+  final WeatherRepository _weatherRepository;
+
+  WeatherCubit(this._weatherRepository) : super(WeatherInitial());
+
+  Future<void> getWeather(String cityName) async {
+    try {
+      emit(WeatherLoading());
+      final weather = await _weatherRepository.fetchWeather(cityName);
+      emit(WeatherLoaded(weather));
+    } on NetworkException {
+      emit(WeatherError("Couldn't fetch weather. Is the device online?"));
+    }
+  }
+}
